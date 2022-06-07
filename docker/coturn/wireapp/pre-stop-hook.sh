6d3b3d6d74e1c425920d7d8f461db9b4719b7d80
Molly Miller <molly.miller@wire.com>
2022-06-07 15:31:08 +0100

    Add docker image which includes pre-stop hook script.
    This adds a container image derived from the Debian coturn image which includes
    a pre-stop hook script. This hook script polls the Prometheus metrics endpoint
    and waits until the number of active allocations reaches zero. This can be used
    to detect when traffic is drained from a running coturn instance, in order to
    perform a non-invasive graceful restart.

---
+++ docker/coturn/wireapp/pre-stop-hook.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# FUTUREWORK: this will break with IPv6. this should be fixed so it works with
+# IPv6.
+
+set -uo pipefail
+
+SLEEPTIME=60
+
+host="$1"
+port="$2"
+
+url="http://$host:$port/metrics"
+
+while true; do
+    allocs=$(curl -s "$url" | grep -E '^turn_active_allocations' | cut -d' ' -f2)
+    if [ "$?" != 0 ]; then exit 1; fi
+
+    if [ -z "$allocs" ]; then exit 0; fi
+    if [ "$allocs" = 0 ]; then exit 0; fi
+
+    sleep "$SLEEPTIME"
+done
