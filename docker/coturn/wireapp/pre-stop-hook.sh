237ebac139a3b37b5589edae4f3196847c217d2f
Molly Miller <molly.miller@wire.com>
2022-06-08 11:52:33 +0100

    Add status reporting to pre-stop hook script.

--- docker/coturn/wireapp/pre-stop-hook.sh
+++ docker/coturn/wireapp/pre-stop-hook.sh
@@ -12,12 +12,24 @@ port="$2"
 
 url="http://$host:$port/metrics"
 
+echo "Polling coturn status on $url"
+
 while true; do
     allocs=$(curl -s "$url" | grep -E '^turn_active_allocations' | cut -d' ' -f2)
-    if [ "$?" != 0 ]; then exit 1; fi
+    if [ "$?" != 0 ]; then
+        echo "Could not retrieve metrics from coturn!"
+        exit 1
+    fi
 
-    if [ -z "$allocs" ]; then exit 0; fi
-    if [ "$allocs" = 0 ]; then exit 0; fi
+    if [ -z "$allocs" ]; then
+        echo "No more active allocations, exiting"
+        exit 0
+    fi
+    if [ "$allocs" = 0 ]; then
+        echo "No more active allocations, exiting"
+        exit 0
+    fi
 
+    echo "Active allocations remaining, sleeping for $SLEEPTIME seconds"
     sleep "$SLEEPTIME"
 done
