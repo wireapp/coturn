5eaba428e7ee3c726952f288c213d2ee29f805a2
jschaul <jschaul@users.noreply.github.com>
2024-06-19 18:18:48 +0200

    cleanup and refactor into functions

--- docker/coturn/wireapp/pre-stop-hook.sh
+++ docker/coturn/wireapp/pre-stop-hook.sh
@@ -17,41 +17,39 @@ coturn_pid=$(pgrep -f "$coturn_exe" | head -n 1)
 
 function log(){
     msg=$1
+    # send log output of the preStop hook to stdout of
+    # the main turnserver process, so they show up in the
+    # normal logs of the pod.
     echo "PRESTOP: $msg" > "/proc/$coturn_pid/fd/1"
 }
 
-log "Polling coturn status on $url"
+function getAllocations(){
+    log "Polling coturn status on $url ..."
+    allocs=$(curl -s "$url" | grep -E '^turn_total_allocations' | cut -d' ' -f2)
+    if [ -z "$allocs" ]; then
+        # nobody used the coturn server yet, which means the metric is absent from the output, in which case default to 0.
+        allocs=0
+    fi
+    # Note: there can be multiple allocation counts, e.g.
+    # turn_total_allocations{type="UDP"} 0
+    # turn_total_allocations{type="TCP"} 0
+    # So we need to sum the counts before comparing with 0.
+    sum=0
+    for num in $allocs; do
+        (( sum += num ))
+    done
+    log "Active remaining turn_allocations: $sum"
+}
 
-allocs=$(curl -s "$url" | grep -E '^turn_total_allocations' | cut -d' ' -f2)
-if [ -z "$allocs" ]; then
-    # nobody used the coturn server yet, which means the metric is absent, default to 0.
-    allocs=0
-fi
-# Note: there can be multiple allocation counts, e.g.
-# turn_total_allocations{type="UDP"} 0
-# turn_total_allocations{type="TCP"} 0
-# So we need to sum the counts before comparing with 0.
-sum=0
-for num in $allocs; do
-    (( sum += num ))
-done
-
-log "Active allocations: [$sum] remaining."
+getAllocations
 
 # Invoke drain mode (https://github.com/wireapp/coturn/pull/12)
 log "Sending signal to drain coturn..."
 pkill -f --signal SIGUSR1 "$coturn_exe"
 
-sleep 1
-allocs=$(curl -s "$url" | grep -E '^turn_total_allocations' | cut -d' ' -f2)
-if [ -z "$allocs" ]; then
-    # nobody used the coturn server yet, which means the metric is absent, default to 0.
-    allocs=0
-fi
-log "After drain signal allocs: $allocs"
-
 while pgrep -f "$coturn_exe" > /dev/null; do
-    log "$coturn_exe is still running. Waiting..."
+    log "$coturn_exe is still running. Checking allocations then sleeping..."
+    getAllocations
     sleep $SLEEPTIME
 done
 log "$coturn_exe seems to have finished draining. Exiting."
